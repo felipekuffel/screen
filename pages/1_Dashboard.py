b883bc1d95716aa10f9431514766ec304fda310a
felipekuffel <137618716+felipekuffel@users.noreply.github.com>
2025-05-13 13:16:47 -0300

    Add files via upload

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -6,49 +6,32 @@ import numpy as np
 from finvizfinance.screener.overview import Overview
 import plotly.graph_objects as go
 import plotly.express as px
+import time
 from plotly.subplots import make_subplots
 import datetime
+import pyrebase
 import firebase_admin
 from firebase_admin import credentials, auth as admin_auth
+import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from cryptography.hazmat.primitives import serialization
+from streamlit_autorefresh import st_autorefresh
 import io
+import sys
 from contextlib import redirect_stdout, redirect_stderr
 import re
 from finvizfinance.screener.overview import Overview
+import requests
+
+st.set_page_config(layout="wide")
 
 # Verifica se o usuário está autenticado
 if "logged_in" not in st.session_state or not st.session_state.logged_in:
     st.warning("⚠️ Você precisa estar logado para acessar esta página.")
     st.link_button("🔐 Ir para Login", "/")
     st.stop()
-    
 
-
-try:
-    key = st.secrets["firebase_admin"]["private_key"]
-    serialization.load_pem_private_key(key.encode(), password=None)
-except Exception as e:
-    st.error(f"❌ Erro na chave privada: {e}")
-    st.stop()
-
-if not firebase_admin._apps:
-    try:
-        cred = credentials.Certificate(dict(st.secrets["firebase_admin"]))
-        firebase_admin.initialize_app(cred, {
-            "databaseURL": st.secrets["databaseURL"]
-        })
-    except Exception as e:
-        st.error(f"Erro ao inicializar Firebase: {e}")
-        st.stop()
-
-if "user" not in st.session_state or "localId" not in st.session_state.user:
-    st.error("Usuário não autenticado corretamente.")
-    st.stop()
-
-
-# --- Estilo e cabeçalho ---
 st.markdown("""
     <style>
     [data-testid="stSidebar"]::before {
@@ -70,6 +53,7 @@ st.markdown("""
         <h5 style="margin-top: 8px;">Realize a busca no menu lateral</h5>
     </div>
 """, unsafe_allow_html=True)
+
 
 
 # --- Função de earnings detalhado ---
@@ -507,6 +491,12 @@ if highlow_filter:
     filters_dict["52-Week High/Low"] = highlow_filter
 
 
+# ✅ Botão de logout sempre visível
+if st.sidebar.button("🚪 Sair"):
+    for key in list(st.session_state.keys()):
+        del st.session_state[key]
+    st.rerun()
+
 
 def inserir_preco_no_meio(niveis: list, preco: float) -> pd.DataFrame:
     df = pd.DataFrame(niveis)
@@ -552,7 +542,7 @@ if executar:
 
 # Criação do dicionário de filtros dinâmicos já foi feita acima
     with redirect_stdout(f), redirect_stderr(f):
-        with st.spinner(" Buscando ativos..."):
+        with st.spinner("🔄 Buscando ativos..."):
             screener = Overview()
             screener.set_filter(filters_dict=filters_dict)
             tickers_df = screener.screener_view()
