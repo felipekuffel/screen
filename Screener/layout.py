2b16ed33af61e697ec1dd08b15fdeed66160b7a9
felipekuffel <137618716+felipekuffel@users.noreply.github.com>
2025-05-15 15:38:38 -0300

    Update layout.py

--- Screener/layout.py
+++ Screener/layout.py
@@ -11,17 +11,17 @@ if "logged_in" not in st.session_state or not st.session_state.logged_in:
 
 def aplicar_zoom(percentual=70):
     escala = percentual / 100
+    proporcao = 100 / escala  # Ex: 1/0.7 = 143%
 
     st.markdown(f"""
         <style>
         [data-testid="stApp"] {{
-            zoom: {percentual}%;
-            -moz-transform: scale({escala});
-            -moz-transform-origin: top left;
-            -webkit-transform: scale({escala});
-            -webkit-transform-origin: top left;
             transform: scale({escala});
             transform-origin: top left;
+            width: {proporcao:.0f}%;
+        }}
+        html, body {{
+            overflow-x: hidden;
         }}
         </style>
     """, unsafe_allow_html=True)
